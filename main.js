50434cc8179dd6223c13c9b71acd3b2641fa7e3d
phaze-the-dumb <phaze@duck.com>
2023-07-20 11:36:44 +0100

    e

---
+++ main.js
@@ -0,0 +1,83 @@
+const fs = require('fs');
+
+class PNGImage{
+  /***
+   * @param {Buffer | String} buffOrPath
+   */
+  constructor( buffOrPath ){
+    this.width = null;
+    this.height = null;
+    this.bitDepth = null;
+    this.colourType = null;
+    this.compressionMethod = null;
+    this.filterMethod = null;
+    this.interlaceMethod = null;
+    this.meta = {};
+
+    let buff = buffOrPath;
+
+    if(!buffOrPath instanceof Buffer)
+      buff = fs.readFileSync(buffOrPath);
+
+    // Check for 8-byte signature, if it doesn't have it then its not a png.
+    if(
+      buff[0] !== 0x89 || buff[1] !== 0x50 || buff[2] !== 0x4E || buff[3] !== 0x47 ||
+      buff[4] !== 0x0D || buff[5] !== 0x0A || buff[6] !== 0x1A || buff[7] !== 0x0A
+    ) throw new Error('Not a PNG file');
+
+    this.readPngChunk(8, buff);
+  }
+
+  /***
+   * @param {Number} startByte
+   * @param {Buffer} buffer
+   */
+  readPngChunk( startByte, buffer ){
+    let buff = buffer.subarray(startByte, buffer.length);
+    
+    let length = this.readNum(buff.subarray(0, 4));
+    let type = String.fromCharCode(...buff.subarray(4, 8));
+
+    switch(type){
+      case 'IHDR':
+        this.width = this.readNum(buff.subarray(8, 12));
+        this.height = this.readNum(buff.subarray(12, 16));
+        this.bitDepth = buff[16];
+        this.colourType = buff[17];
+        this.compressionMethod = buff[18];
+        this.filterMethod = buff[19];
+        this.interlaceMethod = buff[20];
+
+        // I'll probably do checksums at some point, cba right now
+        break;
+      case 'iTXt':
+        let d = String.fromCharCode(...buff.subarray(8, 8 + length)).split("\x00");
+        d = d.filter(x => x !== '');
+
+        try{
+          this.meta[d[0]] = JSON.parse(d[1]);
+        } catch(e){
+          this.meta[d[0]] = d[1];
+        }
+        
+        break;
+    }
+
+    if(type !== 'IEND')
+      this.readPngChunk(length + 12, buff);
+  }
+
+  /***
+   * @param {Array<Number>} hex
+   */
+  readNum( hex ){
+    let num = 0;
+
+    for(let i = 0; i < hex.length; i++)
+      num += hex[i] * Math.pow(16, (i - hex.length + 1) * -2);
+
+    return num;
+  }
+}
+
+module.exports = { PNGImage }
---
+++ readme.md
@@ -0,0 +1,31 @@
+# PNG Metadata Lib
+
+Example:
+
+```js
+const { PNGImage } = require('png-metadata');
+
+let png = new PNGImage('FILE PATH OR BUFFER');
+
+console.log(png)
+
+/*
+PNGImage {
+  width: 2560,
+  height: 1440,
+  bitDepth: 8,
+  colourType: 2,
+  filterMethod: 0,
+  interlaceMethod: 0,
+  meta: {
+    Description: {
+      application: 'VRCX',
+      version: 1,
+      author: [Object],
+      world: [Object],
+      players: [Array]
+    }
+  }
+}
+*/
+```
